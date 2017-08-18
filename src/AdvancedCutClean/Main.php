836faac595c7ddec8003f17a1bf0517db0df8f3f
Angel <bubblicious712@gmail.com>
2017-08-18 19:40:30 -0400

    Update Main.php
    Fixed all errors

--- src/AdvancedCutClean/Main.php
+++ src/AdvancedCutClean/Main.php
@@ -1,51 +1,40 @@
 <?php
+
 namespace AdvancedCutClean;
 
 use pocketmine\block\Block;
-use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\item\Item;
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener {
-
-public function onEnable() {
-  $this->getLogger()->info("AdvancedCutClean enabled! Made by inovamc");
-
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
+  
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("AdvancedCutClean enabled! Made by inovamc");
  }
-
-public function onDisable() {
-  $this->getLogger()->info("AdvancedCutClean Disabled!"); 
-
-} 
-
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
-
- }
-
-public function onBreak(BlockBreakEvent $event) {
-
-$player = $event->getPlayer();
-$block = $event->getBlock();
-switch ($block->getId()) {
-
-case Block::GOLD_ORE: 
-  //$event->setDrops(array(Item::get(Item::GOLD_INGOT, 0, 1)));
- $player->getInventory()->addItem(Item::get(Item::GOlD_INGOT, 0, 1); 
-
-  break;
-
-case Block::IRON_ORE: 
-  //$event->setDrops(array(Item::get(Item::IRON_INGOT, 0, 1)));
- $player->getInventory()->addItem(Item::get(Item::IRON_INGOT, 0, 1);
-
-  break;
-
-case Block::COAL_ORE: 
-  //$event->setDrops(array(Item::get(Item::TORCH, 0, 4))); 
- $player->getInventory()->addItem(Item::get(Item::TORCH, 0, 1); 
-
-  break;
-
+  
+  public function onDisable() {
+    $this->getLogger()->info("AdvancedCutClean Disabled!");
   }
- }
+  
+  public function onBreak(\pocketmine\event\block\BlockBreakEvent $event) {
+    $player = $event->getPlayer();
+    $block = $event->getBlock();
+    switch ($block) {
+      
+      case Block::GOLD_ORE: 
+      $player->getInventory()->addItem(Item::get(Item::GOlD_INGOT, 0, 1)); 
+      return true;
+      break;
+                                         
+      case Block::IRON_ORE: 
+      $player->getInventory()->addItem(Item::get(Item::IRON_INGOT, 0, 1));
+      return true;
+      break;
+      
+      case Block::COAL_ORE: 
+      $player->getInventory()->addItem(Item::get(Item::TORCH, 0, 1)); 
+      return true;
+      break;
+    }
+  }
+}
